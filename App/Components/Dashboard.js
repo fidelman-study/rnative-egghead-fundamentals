19472addd419ec7919f1169bca83023813d20a7a
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-07-31 19:51:06 +0200

    Render short info

--- App/Components/Dashboard.js
+++ App/Components/Dashboard.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import {
   Text,
   View,
-  StyleSheet
+  StyleSheet,
+  Image,
+  TouchableHighlight
 } from 'react-native';
 
 const styles = StyleSheet.create({
@@ -21,10 +23,70 @@ const styles = StyleSheet.create({
 });
 
 class Dashboard extends Component {
+  constructor() {
+    super();
+
+    this.goToProfile = this.goToProfile.bind(this);
+    this.goToRepos = this.goToRepos.bind(this);
+    this.goToNotes = this.goToNotes.bind(this);
+  }
+
+  goToProfile() {
+
+  }
+
+  goToRepos() {
+
+  }
+
+  goToNotes() {
+
+  }
+
+  static makeBackground(btn) {
+    const obj = {
+      flexDirection: 'row',
+      alignSelf: 'stretch',
+      justifyContent: 'center',
+      flex: 1
+    };
+
+    if(btn === 0) {
+      obj.backgroundColor = '#48BBEC'
+    } else if (btn === 1) {
+      obj.backgroundColor = '#E77AAE'
+    } else {
+      obj.backgroundColor = '#758BF4'
+    }
+
+    return obj;
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>This is the</Text>
+        <Image source={{ uri: this.props.userInfo.avatar_url }} style={styles.image}/>
+        <TouchableHighlight
+          style={Dashboard.makeBackground(0)}
+          onPress={this.goToProfile}
+          underlayColor="#88D4F5"
+        >
+          <Text style={styles.buttonText}>View Profile</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={Dashboard.makeBackground(1)}
+          onPress={this.goToRepos}
+          underlayColor="#88D4F5"
+        >
+          <Text style={styles.buttonText}>View Repos</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={Dashboard.makeBackground(2)}
+          onPress={this.goToNotes}
+          underlayColor="#88D4F5"
+        >
+          <Text style={styles.buttonText}>View Notes</Text>
+        </TouchableHighlight>
       </View>
     );
   }
