d42321451e6f7749ccc8c1e9ee8503843f70b23a
Andrei Fidelman <andrei.fidelman@actum.cz>
2017-08-01 23:24:49 +0200

    Methods to connect Firebase

--- App/Utils/api.js
+++ App/Utils/api.js
@@ -8,5 +8,19 @@ export default {
     username = username.toLowerCase().trim();
     const url = `https://api.github.com/users/${username}`;
     return fetch(url).then((res) => res.json());
+  },
+  getNotes(username) {
+    username = username.toLowerCase().trim();
+    const url = `https://github-saver-2e4fa.firebaseio.com/${username}.json`;
+    return fetch(url).then((res) => res.json());
+  },
+  addNote(username, note) {
+    username = username.toLowerCase().trim();
+    const url = `https://github-saver-2e4fa.firebaseio.com/${username}.json`;
+    return fetch(url, {
+      method: 'post',
+      body: JSON.stringify(note)
+    }).then((res) => res.json());
   }
+
 };
